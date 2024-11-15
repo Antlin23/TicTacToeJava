78a1ce6b1f095ae6bc038e0bb245eae83c602351
Anton Lindström <anton.jumkil@gmail.com>
2024-11-15 10:29:14 +0100

    basic functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        char[] gamePlan = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+        System.out.println("Välkommen till Antons tre i rad!");
+        System.out.print("Spelare ett namn: ");
+
+        Player player1 = new Player(scanner.nextLine(), 'X');
+
+        System.out.print("Spelare två namn: ");
+        Player player2 = new Player(scanner.nextLine(), 'O');
+
+        System.out.println(player1.Name + " du är " + player1.Symbol + ", " + player2.Name + " är " + player2.Symbol);
+
+        Player activePlayer = player1;
+
+        System.out.println(gamePlan[0] + " " + gamePlan[1] + " " + gamePlan[2]);
+        System.out.println(gamePlan[3] + " " + gamePlan[4] + " " + gamePlan[5]);
+        System.out.println(gamePlan[6] + " " + gamePlan[7] + " " + gamePlan[8]);
+
+        System.out.println(activePlayer.Name + ", vart vill du spela? 1-9");
+
+        Integer activePLayersMove = scanner.nextInt() - 1;
+
+        gamePlan[activePLayersMove] = activePlayer.Symbol;
+
+        System.out.println(gamePlan[0] + " " + gamePlan[1] + " " + gamePlan[2]);
+        System.out.println(gamePlan[3] + " " + gamePlan[4] + " " + gamePlan[5]);
+        System.out.println(gamePlan[6] + " " + gamePlan[7] + " " + gamePlan[8]);
+
+        boolean continueGame = true;
+        while(continueGame){
+            if(true){
+                
+            }else{
+                continueGame = false;
+            }
+        }
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,8 @@
+public class Player {
+    public String Name;
+    public char Symbol;
+    public Player(String name, char symbol){
+        this.Name = name;
+        this.Symbol = symbol;
+    }
+}
